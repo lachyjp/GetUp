ce4045ee4a01abdaeb2aaefa07a2d8d52a775e82
lachyjp <inbox@lachlanpaull.com>
2022-05-06 13:54:49 +1000

    Styling and more

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const txnURL = "https://api.up.com.au/api/v1/transactions";
 const httpAccStatus = ['acc-status-successful', 'acc-status-failed'];
 const httpTxnStatus = ['txn-status-successful', 'txn-status-failed'];
 const authStatus = ['auth-status-successful', 'auth-status-failed'];
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com',
+		'X-RapidAPI-Key': 'da494ecdc7msheef92a697e197f3p11fd65jsnf313d41588c9'
+	}
+};
 
 function submitUpKey() {
     let upKey = document.getElementById("upKeyInput").value;
@@ -21,7 +28,6 @@ async function getAcc(upKey) {
         let obj = await response.json();
         console.log(obj);
         document.getElementById(httpAccStatus[0]).innerHTML = `${response.status}: Success!`;
-        console.log(obj.length)
 
         //ADD ACCOUNTS
         let balanceTotal = 0;
@@ -59,9 +65,10 @@ async function getTxn(upKey) {
         var txn = [];
 
         //ADD TRANSACTIONS
-        for (i = 0; i < obj.data.length; i++) {
+        for (i = 0; i < 20; i++) {
+            //let txnIcon = txnURL
             let txnDesc = obj.data[i].attributes.description;
-            let txnValType = ""
+            let txnValType = "";
             let txnVal = parseFloat(obj.data[i].attributes.amount.value);
             let txnStatus = obj.data[i].attributes.status;
             let txnRawDate =  obj.data[i].attributes.createdAt;
@@ -71,35 +78,33 @@ async function getTxn(upKey) {
 
             //formatting data
             if (txnVal < 0) {
-                txnVal = txnVal * -1
+                txnVal = txnVal * -1;
                 txnValType = "";
             }
             else {
                 txnValType = "+";
             };
-
             if (txnRawText == null) {
                 txnRawText = "";
-            }
-
+            };
             if (txnMessage == null) {
                 txnMessage = "";
-            }
-
+            };
             if (txnRoundUp == null) {
                 txnRoundUp = "";
-            }
+            };
 
+            //new array
             let txnArray = [];
             txnArray[i] = new Transaction(txnDesc, txnValType, txnVal, txnStatus, txnRawDate, txnRawText, txnMessage, txnRoundUp);
             txn.push(txnArray[i])
-
-            newTextNode(`${txn[i].desc}, ${txn[i].type}$${txnVal}, ${txn[i].status}, ${txn[i].date}, ${txn[i].text} ${txn[i].msg} ${txn[i].roundup}`, "p", "activity");
-
-            
+           
         };
+
+        for (i = 0; i < txn.length; i++) {
+            newTextNode(`${txn[i].desc}, ${txn[i].type}$${txn[i].val}, ${txn[i].status}, ${txn[i].date}, ${txn[i].text} ${txn[i].msg} ${txn[i].roundup}`, "p", "activity");
+        }
         console.log(txn);
-        newTextNode()
 
     } else {
         //ERROR
